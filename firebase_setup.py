8001544e6ddf7cd9b31c14807e59a63400561423
Lucas-Gemmell <chickenfingers257@gmail.com>
2024-08-21 10:50:09 -0600

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,12 +1,2 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
 
-street_cred = credentials.Certificate("C:\\Users\\Lucas Gemmell\\Downloads\\testing-6fa60-firebase-adminsdk-6ocwe-c3ca51ff3d.json")
-
-firebase_admin.initialize_app(street_cred)
-
-db = firestore.client()
-
-def return_db():
-    return db
     
